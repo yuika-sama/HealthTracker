c9c17ddb13c1aa99d77be8522a583a145051183e
Nguyen Duc Anh <nguyenanhduc2938@gmail.com>
2026-07-09 12:37:05 +0700

    feat: create activity repository

---
+++ app/src/main/java/com/yuika/healthtracker/data/repository/ActivityRepositoryImpl.kt
@@ -0,0 +1,46 @@
+package com.yuika.healthtracker.data.repository
+
+import com.yuika.healthtracker.data.local.dao.ActivityDao
+import com.yuika.healthtracker.data.local.entity.ActivityEntity
+import com.yuika.healthtracker.domain.repository.ActivityRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+
+@Singleton
+abstract class ActivityRepositoryImpl @Inject constructor(
+    private val activityDao: ActivityDao
+) : ActivityRepository
+{
+    override suspend fun insertActivity(activity: ActivityEntity): Long
+    {
+        return activityDao.insertActivity(activity)
+    }
+
+    override suspend fun updateActivity(activity: ActivityEntity)
+    {
+        return activityDao.updateActivity(activity)
+    }
+
+    override suspend fun deleteActivity(activity: ActivityEntity)
+    {
+        return activityDao.deleteActivity(activity)
+    }
+
+    override fun getActivitiesByDate(
+        userId: Int,
+        dateText: String
+    ): Flow<List<ActivityEntity>>
+    {
+        return activityDao.getActivitiesByDate(userId, dateText)
+    }
+
+    override fun getTotalBurnedCaloriesByDate(
+        userId: Int,
+        dateText: String
+    ): Flow<Int?>
+    {
+        return activityDao.getTotalBurnedCaloriesByDate(userId, dateText)
+    }
+}
---
+++ app/src/main/java/com/yuika/healthtracker/domain/repository/ActivityRepository.kt
@@ -0,0 +1,17 @@
+package com.yuika.healthtracker.domain.repository
+
+import com.yuika.healthtracker.data.local.entity.ActivityEntity
+import kotlinx.coroutines.flow.Flow
+
+interface ActivityRepository
+{
+    suspend fun insertActivity(activity: ActivityEntity): Long
+
+    suspend fun updateActivity(activity: ActivityEntity)
+
+    suspend fun deleteActivity(activity: ActivityEntity)
+
+    fun getActivitiesByDate(userId: Int, dateText: String): Flow<List<ActivityEntity>>
+
+    fun getTotalBurnedCaloriesByDate(userId: Int, dateText: String): Flow<Int?>
+}
